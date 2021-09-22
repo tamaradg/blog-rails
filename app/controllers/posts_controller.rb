d7b0b5b34ccf5dbaab3b6106604012871a5bcc37
Tamara de Gaetano <tamara.degaetano@yahoo.com>
2021-09-22 02:43:37 +0200

    add simple form new+ views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,25 @@ def index
   @posts = Post.all
 end
 
+def new
+  @post = Post.new
+end
 
+def create
+  @post = Post.new(post_params)
+  @post.user = current_user
+  if @post.save
+    redirect_to post_path(@post)
+  else
+  render :new
+  end
+end
+
+private
+
+def post_params
+  params.require(:post)
+        .permit(:title, :body, :user_id)
+end
 
 end
