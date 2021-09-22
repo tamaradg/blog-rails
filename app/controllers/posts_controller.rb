5ed4e8ba44a284187d9e4742638dd018c3cd7877
Tamara de Gaetano <tamara.degaetano@yahoo.com>
2021-09-23 00:24:02 +0200

    destroy path done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-before_action :set_post, only: [:edit, :update]
+before_action :set_post, only: [:edit, :update, :destroy]
 
 def index
   @posts = Post.all
@@ -28,6 +28,10 @@ def update
   redirect_to posts_path(@post)
 end
 
+def destroy
+  @post.destroy
+  redirect_to posts_path
+end
 
 
 private
